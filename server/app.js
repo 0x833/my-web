1ff787f80afc5241ac666e67a9a644705a8391f1
Erick Meyer <erickmmeyer8@gmail.com>
2016-02-15 15:50:32 -0800

    Basic response to request

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('My site is currently being built! :(');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
 });
