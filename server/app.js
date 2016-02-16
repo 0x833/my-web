109ac208f973dca143bedfc2c79beaf5f7777335
Erick Meyer <erickmmeyer8@gmail.com>
2016-02-15 17:00:13 -0800

    stuff

--- server/app.js
+++ server/app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('My site is currently being built! :(');
 });
 
-app.listen(80, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 8000!');
 });
