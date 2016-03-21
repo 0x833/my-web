b51717111b35c0d1552d0685234ad3a6543ffa6a
Erick Meyer <erickmmeyer8@gmail.com>
2016-03-21 01:45:44 -0700

    New updates

--- app/controllers.js
+++ app/controllers.js
@@ -40,3 +40,40 @@ webApp.controller('aboutController', [
 		});
 	}
 ]);
+
+webApp.controller('projectsCtrl', [
+	'$scope',
+	'$log',
+	function($scope, $log){
+		$scope.projectList = [
+			{
+				name: "Interactive MATLAB Tutorial",
+				description: "Web application, using MEAN stack."
+			},
+			{
+				name: "Distance Vector Algorithm",
+				description: "Router protocol, using routing tables to determine best path."
+			},
+			{
+				name: "Custom Proxy",
+				description: "Python based proxy that reads incoming and outgoing headers and modifies various fields."
+			},
+			{
+				name: "Synchronizers",
+				description: "Basic implementations of simple concurrency Synchronizers"
+			},
+			{
+				name: "Post Script Interpreter",
+				description: "Follows post-script grammar and executes programs using Python"
+			}
+		];
+	}
+]);
+
+webApp.controller('aboutCtrl', [
+	'$scope',
+	'$log',
+	function($scope, $log){
+
+	}
+]);
