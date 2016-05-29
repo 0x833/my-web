5999d49626f90cc56a64001941f0933f392cac66
Erick Meyer <erickmmeyer8@gmail.com>
2016-05-29 13:11:16 -0700

    New index

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ var app     = express();
 var path    = require("path");
 
 //Send static files from client
-app.use(express.static(__dirname + '/app'));
+//app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/fastfit'));
 
 app.get('*',function(req,res){
+  res.sendFile(__dirname + '/fastfit/index.html');
+  //__dirname : It will resolve to your project folder.
+});
+
+/*
+app.get('/erickmeyer',function(req,res){
   res.sendFile(__dirname + '/app/index.htm');
   //__dirname : It will resolve to your project folder.
 });
+*/
 
 app.listen(8080, function(){
 	console.log("Listening on port 8080");
